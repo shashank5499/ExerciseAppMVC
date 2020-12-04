18adef6f34b8bcc841fd74723b750d0d6ca376dd
Shashank Thakur <shashank.thakur.1848816@gmail.com>
2020-12-04 16:41:07 +0530

    Second Commit

---
+++ WebAppMVC/WebAppMVC/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMVC.Models
+{
+    public class Students
+    {
+        public int SID { get; set; }
+        public string SName { get; set; }
+        public DateTime SDOB { get; set; }
+        public string SContact { get; set; }
+    }
+}
